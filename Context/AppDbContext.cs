aa0c0199677753c275f7ad4c9b103a41bd58b195
Anna Klara <anna.silva5@portalsesisp.org.br>
2024-03-22 09:57:53 -0300

    apifuncionario

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+
+using apifuncionario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiitens.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base (options)
+    {
+
+    }
+
+    public DbSet<Funcionario> Funcionarios  {get;set;}
+    public DbSet<Setor> Setors {get;set;}
+}
